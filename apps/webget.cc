c5f0802cb4a72ce4f8923f82e04ffc498cc26452
ASS <1037997956@qq.com>
2023-04-09 02:38:19 -0700

    change webget to my tcp implementation

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "socket.hh"
+#include "tcp_sponge_socket.hh"
 #include "util.hh"
 
 #include <cstdlib>
@@ -16,7 +16,7 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-    TCPSocket sock;
+    CS144TCPSocket sock;
     string service = "http";
     string cmd = "GET " + path + " HTTP/1.1" + "\r\n" + "Host: " + host + + "\r\n" + "Connection: close" + "\r\n\r\n";
     sock.connect(Address(host, service));
@@ -28,7 +28,8 @@ void get_URL(const string &host, const string &path) {
         result = sock.read();
         cout << result;
     }
-    sock.close();
+    // sock.close();
+    sock.wait_until_closed();
 }
 
 int main(int argc, char *argv[]) {
